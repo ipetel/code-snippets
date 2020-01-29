6c869552a27b6a3b974751d26504642fb662a73b
Idan Petel <ipetel@gmail.com>
2020-01-29 18:07:56 +0200

    Create safely-retrieve-value-from-nested-dict.py

---
+++ Python3-functions/safely-retrieve-value-from-nested-dict.py
@@ -0,0 +1,26 @@
+'''
+	get value from Dict in safe way - this function includes a simple example to understand how to use it.
+'''
+import json
+
+data_json='{"key_10":{"key_20":{"key_30":11,"key_31":"None","key_32":{"key_40":"qwe","key_41":"ewq"}},"key_21":{"key_30":22,"key_31":33}},"key_11":"abcd"}';
+data_dict=json.loads(data_json)
+
+def get_val_from_dict_by_key_list(data_dict,key_list):
+	if isinstance(data_dict,dict):
+		dynamic_dict_path=data_dict
+		for key in key_list:
+			if key in dynamic_dict_path:
+				dynamic_dict_path=dynamic_dict_path[key]
+			else:
+				print('key "{}" is not exists in this level, the keys available are: {}'.format(key,list(dynamic_dict_path.keys())))
+				return None  
+
+		return dynamic_dict_path
+	else:
+		print('"data_dict" is not "dict" type!')
+		return None
+
+
+val_needed=get_val_from_dict_by_key_list(data_dict,['key_10','key_20','key_32','key_41'])
+print(val_needed)
