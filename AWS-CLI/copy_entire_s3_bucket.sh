4e057e58e8bf04caf7fb61004af56b14a6a197d9
Idan Petel <ipetel@gmail.com>
2020-02-06 18:46:53 +0200

    Update copy_entire_s3_bucket.sh

--- AWS-CLI/copy_entire_s3_bucket.sh
+++ AWS-CLI/copy_entire_s3_bucket.sh
@@ -5,5 +5,5 @@
 # run this commend in your command line to copy from one bucket to the other:
 aws s3 sync s3://SOURCE-BUCKET-NAME s3://DESTINATION-BUCKET-NAME --source-region SOURCE-REGION-NAME --region DESTINATION-REGION-NAME
 
-#run this commmend in your command line to copy from one buket to local machine:
+#run this commmend in your command line to copy from an S3 bucket to local machine in specific path:
 aws s3 sync s3://SOURCE-BUCKET-NAME /Users/some_name/folder1/ --source-region SOURCE-REGION-NAME
