a81a2e3e60ecf7ae0707369777120d20c413e58f
Idan Petel <ipetel@gmail.com>
2020-02-12 16:33:06 +0200

    Create Lambda_sendArguments_to_GlueJob.py

---
+++ PySpark/Lambda_sendArguments_to_GlueJob.py
@@ -0,0 +1,17 @@
+'''
+  this script is for invoke Glue Job from Lambda function using Arguments by using boto3 SDK
+'''
+
+### in the Lambda function:
+
+import boto3
+glue_client = boto3.client('glue')
+response = glue_client.start_job_run(
+  JobName='<GLUE_JOB_NAME>',
+  Arguments={'--KEY_1':'VAL_1','--KEY_2':'VAL_2'}
+)
+
+### in the Glue Job (PySpark)
+
+args = getResolvedOptions(sys.argv, ['JOB_NAME','<KEY_1>','<KEY_2>'])
+KEY_1_VAL=args['<KEY_1>']
