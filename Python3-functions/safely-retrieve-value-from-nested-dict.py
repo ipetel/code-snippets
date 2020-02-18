6c210448dffbe87488bd5c7b5c69ac70f64aa628
Idan Petel <ipetel@gmail.com>
2020-02-18 11:07:21 +0200

    Update safely-retrieve-value-from-nested-dict.py

--- Python3-functions/safely-retrieve-value-from-nested-dict.py
+++ Python3-functions/safely-retrieve-value-from-nested-dict.py
@@ -10,7 +10,7 @@ def get_val_from_dict_by_key_list(data_dict,key_list):
 	if isinstance(data_dict,dict):
 		dynamic_dict_path=data_dict
 		for key in key_list:
-			print(key)
+			#print('[@@@ INFO] - ',key)
 			if isinstance(dynamic_dict_path,str):
 				dynamic_dict_path=json.loads(dynamic_dict_path)
 
@@ -19,12 +19,12 @@ def get_val_from_dict_by_key_list(data_dict,key_list):
 			elif isinstance(dynamic_dict_path, list) and len(dynamic_dict_path)>=int(key):
 				dynamic_dict_path = dynamic_dict_path[key]
 			else:
-				print('key "{}" is not exists in this level, the keys available are: {}'.format(key,list(dynamic_dict_path.keys())))
+				print('[### ERROR] - key "{}" is not exists in this level, the keys available are: {}'.format(key,list(dynamic_dict_path.keys())))
 				return None  
 
 		return dynamic_dict_path
 	else:
-		print('"data_dict" is not "dict" type!')
+		print('[### ERROR] - "data_dict" is not "dict" type!')
 		return None
 
 val_needed=get_val_from_dict_by_key_list(data_dict,['key_10','key_20','key_32','key_41'])
