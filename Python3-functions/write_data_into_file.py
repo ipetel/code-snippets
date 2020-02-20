e0a792e03321426878d10b333ab1b042809da7cd
Idan Petel <ipetel@gmail.com>
2020-02-20 10:05:00 +0200

    Update write_data_into_file.py

--- Python3-functions/write_data_into_file.py
+++ Python3-functions/write_data_into_file.py
@@ -1,7 +1,15 @@
 '''
-  this code is a general way to erite data into file, in this example it is shown on JSON file.
+  this code is a general way to write data into file.
 '''
 
+### write file to txt
+new_path = '<FILE-PATH>'
+file_1 = open(new_path,'w')
+file_1.write(file_list)
+file_1.close()
+
+
+### write file as JSON
 import json
 
 file_list=['a','b','c']
