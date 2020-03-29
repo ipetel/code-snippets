3586dddbda982470c6a35b58921b0bd5be8a4ce5
Idan Petel <ipetel@gmail.com>
2020-03-29 13:58:49 +0300

    Update get_list_of_all_objKeys_in_bucket.py

--- AWS-boto3/S3/get_list_of_all_objKeys_in_bucket.py
+++ AWS-boto3/S3/get_list_of_all_objKeys_in_bucket.py
@@ -1,6 +1,10 @@
 import boto3
 
-s3_client = boto3.resource('s3')
-bucket_resource = s3_client.Bucket('<BUCKET-NAME>')
+try:
+  s3_client = boto3.resource('s3')
+  bucket_resource = s3_client.Bucket('<BUCKET-NAME>')
 
-file_list= [f.key for f in bucket_resource.objects.all()]
+  file_list= [f.key for f in bucket_resource.objects.all()]
+except ValueError:
+  print(f'[### ERROR] - while trying to read file list of "{bucket_name}" - error message: {ValueError}')
+  return None
