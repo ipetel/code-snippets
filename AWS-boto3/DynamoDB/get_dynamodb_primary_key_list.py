f6fce30c6c5c3b384def2891ff6583f342b3d6b4
Idan Petel <ipetel@gmail.com>
2020-03-29 15:01:11 +0300

    Create get_dynamodb_primary_key_list.py

---
+++ AWS-boto3/DynamoDB/get_dynamodb_primary_key_list.py
@@ -0,0 +1,23 @@
+import boto3
+
+def get_dynamodb_primary_key_list(table_name,partition_key,region):
+	try:
+		dynamodb_resource = boto3.resource('dynamodb',region_name=region)
+		table = dynamodb_resource.Table(table_name)
+		
+		response = table.scan(
+			AttributesToGet=[partition_key],
+			Select='SPECIFIC_ATTRIBUTES'
+		)
+		
+		if 'Count' in response and response['Count']>0:
+			return [x['file_name'] for x in response['Items']]
+		else:
+			print(f'[### ERROR] - dynamodb table "{table_name}" scan return empty response')
+			return None
+		
+		print(f'get_dynamodb_primary_key_list response: {type(response)} | {response}')
+		
+	except ValueError:
+		print(f'[### ERROR] - while trying to read data from dynamodb table "{table_name}" - error message: {ValueError}')
+		return None	 
