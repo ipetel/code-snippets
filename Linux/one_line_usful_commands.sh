8759ff11ad282fabf4da0e4a086b71b0a63eee8c
Idan Petel <ipetel@gmail.com>
2020-06-28 13:08:34 +0300

    Update one_line_usful_commands.sh

--- Linux/one_line_usful_commands.sh
+++ Linux/one_line_usful_commands.sh
@@ -7,3 +7,8 @@ truncate -s 0 <FILE_PATH/FILE_NAME>
 # watch "top" for specific command
 #example for <COMMAND_NAME> = python3
 top -c -p $(pgrep -d',' -f <COMMAND_NAME>)
+
+# look for file in folder path with a name like
+
+# look for file name that start with "abc" from the current path
+find . -name abc*
