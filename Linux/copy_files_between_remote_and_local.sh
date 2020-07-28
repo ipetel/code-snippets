027973cc6c062e3a6f7d6dfb289011ca56730204
Idan Petel <ipetel@gmail.com>
2020-07-28 10:01:20 +0300

    Update copy_files_between_remote_and_local.sh

--- Linux/copy_files_between_remote_and_local.sh
+++ Linux/copy_files_between_remote_and_local.sh
@@ -1,7 +1,7 @@
 # copy file from local to remote
   1) open terminal on your local machine
   2) cd "/folder /with /the /.pem file/"
-  3) scp -i "<FILE_NAME>.pem" "<PATH_TO_LOCAL_FILE>" <USER>@<REMOTE_IP>:<NEW_FILE_PATH>
+  3) scp -i "<FILE_NAME>.pem" "<PATH_TO_LOCAL_FILE>" "<USER>@<REMOTE_IP>:<PATH_TO_LOCAL_FILE>"
      example:
      scp -i "this_is_myFile.pem" "/Users/me/log1.log" "ec2-user@12.345.67.89:/home/ec2-user/log1.log"
      
