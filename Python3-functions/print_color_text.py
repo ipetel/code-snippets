d795e21fc86b5be703596017454418aee8f4ab98
Idan Petel <ipetel@gmail.com>
2020-08-19 11:08:59 +0300

    Create print_color_text.py

---
+++ Python3-functions/print_color_text.py
@@ -0,0 +1,16 @@
+# https://stackoverflow.com/questions/287871/how-to-print-colored-text-in-python
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+def print_color(msg):
+    print(bcolors.WARNING + msg + bcolors.ENDC)
+
+print_color('hello')
