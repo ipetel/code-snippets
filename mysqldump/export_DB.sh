3308449b6054a12f983e7be83f9725bb83629b3b
Idan Petel <ipetel@gmail.com>
2020-08-23 16:55:37 +0300

    Update export_DB.sh

--- mysqldump/export_DB.sh
+++ mysqldump/export_DB.sh
@@ -6,7 +6,7 @@
 # 2. run the next command to create the dump file in the current folder:
   sudo mysqldump -h <DB-ENDPOINT> \
   -u <DB-USER> \
-  -p \
+  -p<DB-PASSWORD> \
   --port=3306 \
   --single-transaction=true \
   --routines \
@@ -15,3 +15,19 @@
 # 3. write or paste the user password and press "enter"   
 
 # Note: the time for creating the dump is depending on the DB size, I have used a small DB (40GB) that took around 30min.   
+
+#________________________________________________________________________________________________________________________________________________
+
+# export DBinto gzip file
+
+  sudo mysqldump -h <DB-ENDPOINT> \
+  -u <DB-USER> \
+  -p<DB-PASSWORD> \
+  --port=3306 \
+  --single-transaction=true \
+  --routines \
+  --triggers \
+  --databases <DB-NAME-TO-DUMP | gzip > <DUMP-FILE-NAME>.sql.gz
+  
+  # when doing mysqldump from RDS you will need to change the owner of the table after unzip it
+  sed -i 's/DEFINER=[^*]*\*/\*/g' <DUMP-FILE-NAME>.sql 
