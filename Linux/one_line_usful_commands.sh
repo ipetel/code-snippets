6e07093c7b4a77b693eb2840c534d9d7da06dbe2
Idan Petel <ipetel@gmail.com>
2020-08-30 13:30:13 +0300

    Update one_line_usful_commands.sh

--- Linux/one_line_usful_commands.sh
+++ Linux/one_line_usful_commands.sh
@@ -1,20 +1,27 @@
 # read last line in file (you can change the number after "-n" to see last x rows)
-tail -n1 -f <FILE_PATH/FILE_NAME>
+  tail -n1 -f <FILE_PATH/FILE_NAME>
 
 # truncate a file 
-truncate -s 0 <FILE_PATH/FILE_NAME>
+  truncate -s 0 <FILE_PATH/FILE_NAME>
 
 # watch "top" for specific command
 #example for <COMMAND_NAME> = python3
-top -c -p $(pgrep -d',' -f <COMMAND_NAME>)
-
-# look for file in folder path with a name like
+  top -c -p $(pgrep -d',' -f <COMMAND_NAME>)
 
 # look for file name that start with "abc" from the current path
-find . -name abc*
+  find . -name abc*
+  find <PATH / . > -type d -name "*<FOLDER-YOU-ARE-LOOKING>*" -print
 
-find <PATH / . > -type d -name "*<FOLDER-YOU-ARE-LOOKING>*" -print
+#_________________________ Ububtu __________________________________________________
 
-# Ububtu - search for only installed packages using apt
-example: sudo apt list -a --installed <PART-OF-PACKAGE-NAME>*
-example: sudo apt list -a --installed libstdc++*
+#### search for only installed packages using apt
+  example: sudo apt list -a --installed <PART-OF-PACKAGE-NAME>*
+  example: sudo apt list -a --installed libstdc++*
+
+#### symbolic link (symlink)
+  #create new link
+  sudo ln -s /usr/bin/python3.5 /usr/bin/python3
+
+  #create update an existed link
+  ls -l /usr/bin/python3
+  sudo ln -sf /usr/bin/python3.5 /usr/bin/python3
