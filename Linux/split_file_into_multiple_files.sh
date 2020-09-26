dbd72ed2807aec8f26d9506449e830466127aefb
Idan Petel <ipetel@gmail.com>
2020-09-26 15:13:58 +0300

    Update split_file_into_multiple_files.sh

--- Linux/split_file_into_multiple_files.sh
+++ Linux/split_file_into_multiple_files.sh
@@ -1,9 +1,15 @@
-# This code will split every file that above the "max_size" value in "dir_path" folder and delete the original file
+# This code will split every file that above the "max_size" value (in Bytes) 
+# in "dir_path" folder and delete the original file
 
-max_size=500M;
-dir_path=/home/ubuntu/tmp/;
-for file in "$(find "$dir_path" -type f -size +"$max_size")"; do
-    echo "$file";
-    split -d -b "$max_size" -a 4 "$file" "$file".;
-    rm "$file";
+dir_path=/home/ubuntu/tmp/*;
+max_size=104857600; #100MB in Bytes
+for file in $dir_path; do 
+    file_size=$(wc -c <"$file")
+    if [ $file_size -gt $max_size ]; then
+        echo "$file size is over 100MB - start spliting the file...";
+        split -d -b "$max_size" -a 4 "$file" "$file".;
+        rm "$file";
+    else
+        echo "$file size is under 100MB";
+    fi
 done
