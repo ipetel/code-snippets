da1668ded5910436d7677a5a118441ce7de52280
Idan Petel <ipetel@gmail.com>
2020-12-05 14:33:36 +0200

    Delete read_and_write_to_local_json_file.py

--- Python3-functions/read_and_write_to_local_json_file.py
+++
@@ -1,9 +0,0 @@
-import json
-
-def read_from_local_json_file(file_path):
-	with open(file_path, 'r') as f:
-		return json.load(f)
-
-def write_into_local_json_file(file_path,data):
-	with open(file_path, 'w') as f:
-		json.dump(data, f)
