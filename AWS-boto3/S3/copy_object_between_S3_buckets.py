7924fd5111b3c1d31490f0c8a3e0e5b9a5a9a02c
Idan Petel <ipetel@gmail.com>
2020-12-05 13:39:21 +0200

    Update copy_object_between_S3_buckets.py

--- AWS-boto3/S3/copy_object_between_S3_buckets.py
+++ AWS-boto3/S3/copy_object_between_S3_buckets.py
@@ -5,7 +5,7 @@
 #            |   / /_/ | / __ \|   |  \  |    |   \  ___/|  | \  ___/|  |__
 #            |___\____ |(____  /___|  /  |____|    \___  >__|  \___  >____/
 #                    \/     \/     \/                 \/          \/
-
+#
 Code Description:       Copy object from one S3 bucket to other S3 bucket (in AWS)
 Auther:                 Idan Petel
 Date:                   Dec 2020
@@ -16,64 +16,60 @@ Environment Variables:  None
 
 import boto3
 import logging
+from functools import wraps
 
-# logging config
+# ___ logging config
 LOG_LEVEL = logging.INFO
 logging.basicConfig(level=LOG_LEVEL, format='### %(levelname)s ### - line: %(lineno)s, msg: %(message)s')
 logger = logging.getLogger()
 
-# --------------------- Using Resource (high-level) Client ---------------------
+# ___ boto3 config
+# uncomment if this code will run on your local with existing AWS config is set up by AWS cli
+'''
+session = boto3.Session(profile_name='idan-sandbox')
+s3_resourceClient = session.resource('s3')
+'''
+# uncomment if this code will run on Lambda with IAM role
+s3_resourceClient = boto3.resource('s3')
+
+# ___ init params
+source_bucket_name = '<BUCKET-NAME-1>'
+target_bucket_name = '<BUCKET-NAME-2>'
+obj_key = '<OBJECT-KEY>'
+
+
+# ___ function_decorator
+def try_except_decorator(function):
+    @wraps(function)
+    def wrapper(*args, **kwargs):
+        try:
+            response = function(*args, **kwargs)
+            logger.info(f'{function.__name__}() function operation was completed successfully')
+            return response
+        except s3_resourceClient.meta.client.exceptions.ClientError as err:
+            logger.warning(f'ClientError: {err}')
+        except s3_resourceClient.meta.client.exceptions.ValidationError as err:
+            logger.warning(f'ValidationError: {err}')
+    return wrapper
+
+# _____________________  Using Resource (high-level) Client _____________________
 # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#S3.Bucket.copy
 
-# boto3 config
-'''
-# uncomment if this code will run on your local with existing AWS config is set up by AWS cli
-session = boto3.Session(profile_name='<PROFILE-NAME>')
-s3_resourceClient = session.resource('s3')
-'''
-# uncomment if this code will run on Lambda with IAM role
-s3_resourceClient = boto3.resource('s3')
-
-
+@try_except_decorator
 def copy_obj_between_buckets_usingResourceClient(source_bucket_name, target_bucket_name, obj_key):
-    try:
-        s3_resourceClient.Bucket(target_bucket_name).copy({'Bucket': source_bucket_name, 'Key': obj_key}, obj_key)
-        logger.info('copy operation was completed successfully')
-    except s3_resourceClient.meta.client.exceptions.ClientError as err:
-        logger.warning(f'ClientError: {err}')
-    except s3_resourceClient.meta.client.exceptions.ValidationError as err:
-        logger.warning(f'ValidationError: {err}')
+    return s3_resourceClient.Bucket(target_bucket_name).copy({'Bucket': source_bucket_name, 'Key': obj_key}, obj_key)
 
 
-source_bucket_name = '<BUCKET-NAME-1>'
-target_bucket_name = '<BUCKET-NAME-2>'
-obj_key = '<OBJECT-KEY>'
-copy_obj_between_buckets_usingResourceClient(source_bucket_name, target_bucket_name, obj_key)
+response = copy_obj_between_buckets_usingResourceClient(source_bucket_name, target_bucket_name, obj_key)
+print(response)
 
-# --------------------- Using Low-Level Client ---------------------
+# _____________________ Using Low-Level Client _____________________
 # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#S3.Client.copy_object
 
-# boto3 config
-'''
-# uncomment if this code will run on your local with existing AWS config is set up by AWS cli
-session = boto3.Session(profile_name='<PROFILE-NAME>')
-s3_resourceClient = session.resource('s3')
-'''
-# uncomment if this code will run on Lambda with IAM role
-s3_resourceClient = boto3.resource('s3')
-
-
+@try_except_decorator
 def copy_obj_between_buckets_usingClient(source_bucket_name, target_bucket_name, obj_key):
-    try:
-        s3_resourceClient.meta.client.copy({'Bucket': source_bucket_name, 'Key': obj_key}, target_bucket_name, obj_key)
-        logger.info('copy operation was completed successfully')
-    except s3_resourceClient.meta.client.exceptions.ClientError as err:
-        logger.warning(f'ClientError: {err}')
-    except s3_resourceClient.meta.client.exceptions.ValidationError as err:
-        logger.warning(f'ValidationError: {err}')
+    return s3_resourceClient.meta.client.copy({'Bucket': source_bucket_name, 'Key': obj_key}, target_bucket_name, obj_key)
 
 
-source_bucket_name = '<BUCKET-NAME-1>'
-target_bucket_name = '<BUCKET-NAME-2>'
-obj_key = '<OBJECT-KEY>'
-copy_obj_between_buckets_usingClient(source_bucket_name, target_bucket_name, obj_key)
+response = copy_obj_between_buckets_usingClient(source_bucket_name, target_bucket_name, obj_key)
+print(response)
