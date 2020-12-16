fbc914b98105193522ec76382b8aabef2c3b47cc
Idan Petel <ipetel@gmail.com>
2020-12-16 18:01:45 +0200

    Update create_newColumn_atBeginning_of_DF.py

--- PySpark/create_newColumn_atBeginning_of_DF.py
+++ PySpark/create_newColumn_atBeginning_of_DF.py
@@ -1,3 +1,7 @@
 from pyspark.sql.functions import lit
 
+# option 1
 df = df.select(lit('value').alias("new_column_header"), "*")
+
+# option 2 - example of using other col that in unix time and convert it into datetime format
+df_2 = df.withColumn("<NEW-COL-NAME>", F.from_unixtime("epoch_timestamp"))
