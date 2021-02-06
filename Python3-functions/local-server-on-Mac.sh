4c3228c1c3d3e7ca3b4fc2920faf4e5105d6cede
Idan Petel <ipetel@gmail.com>
2021-02-06 10:14:44 +0200

    Create local-server-on-Mac.sh

---
+++ Python3-functions/local-server-on-Mac.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# 1. open Terminal
+# 2. cd to the relevent folder with the html files you want to view 
+# 3. python -m SimpleHTTPServer 8000
+# 4. open browser in "localhost:8000/main.html"
